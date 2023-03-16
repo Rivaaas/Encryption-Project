bcfbe485b6a8b67363ca730a1fa44b11ac3420de
Rivaaas <rivaaas.v@gmail.com>
2023-03-16 14:20:36 -0300

    Avance 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
+import { Grid, Button, Typography, Container, Checkbox, FormControlLabel, TextField } from '@mui/material';
 import './App.css';
 
 function App() {
+
+  const [palabra, setPalabra] = useState("")
+  const [palabrax, setPalabrax] = useState("")
+
+
+  // const nombre = "Victor"
+  // const apellido = "Rivas"
+  // const correo = "rivas.v@icloud.com"
+  // const base64 = btoa(nombre)
+  // console.log("ENCRIPTADO", base64)
+  // const nobase64 = atob(base64)
+  // console.log("Desencriptado", nobase64)
+
+
+  const handleSubmit = () => {
+    setPalabrax(btoa(palabra))
+    
+  }
+
+  useEffect(() => {
+    handleSubmit()
+  }, [palabra])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App ">
+      <Grid marginTop="3rem">
+        <Container >
+          <FormControlLabel control={<Checkbox id="check" defaultChecked />} label="Encriptar" />
+        </Container>
+        <Typography marginTop="1rem">
+          <TextField id="palabra" variant="outlined"
+            onChange={(e) => setPalabra(e.target.value)} />
+        </Typography>
+        <Typography marginTop="1rem">
+          <Button variant="contained" type="submit" onSubmit={handleSubmit} color="success">
+            Encriptar
+          </Button>
+        </Typography>
+
+        <div>
+          <h3>-----</h3>
+          <h1>{palabrax}</h1>
+          <h3>-----</h3>
+        </div>
+
+      </Grid>
     </div>
   );
 }
